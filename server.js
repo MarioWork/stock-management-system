b2baeefad24c5e2ff372690ad57260cb25342512
MarioWork <mariovieira.work@hotmail.com>
2023-01-09 16:31:20 -0100

    create first version of server

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
-console.log('Hello World');
-var x;
+const fastify = require('fastify')({ logger: true });
+
+const PORT = '5000';
+
+const start = async () => {
+    try {
+        await fastify.listen(PORT);
+    } catch (error) {
+        fastify.log.error(error);
+        process.exit(1);
+    }
+};
+
+start();
