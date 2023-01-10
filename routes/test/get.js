b46d1c96ba92502692975969d08ef5038ace467d
MarioWork <mariovieira.work@hotmail.com>
2023-01-10 17:04:45 -0100

    play around with json schema

--- routes/test/get.js
+++ routes/test/get.js
@@ -1,5 +1,15 @@
+const S = require('fluent-json-schema');
+
 module.exports = async server => {
-    server.get('/', {}, async (request, reply) => {
+    server.get('/', options, async (request, reply) => {
         await reply.send({ message: 'hello world' });
     });
+};
+
+const options = {
+    schema: {
+        response: {
+            200: S.object().prop('message', S.string()).required()
+        }
+    }
 };
