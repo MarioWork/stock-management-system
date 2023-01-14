2a535121b50ff75f37667b6f9962e99657f51893
MarioWork <mariovieira.work@hotmail.com>
2023-01-14 16:57:09 -0100

    code rearrange

--- routes/test/get.js
+++ routes/test/get.js
@@ -1,15 +1,15 @@
 const S = require('fluent-json-schema');
 
-module.exports = async server => {
-    server.get('/', options, async (request, reply) => {
-        await reply.send({ message: 'hello world' });
-    });
-};
-
 const options = {
     schema: {
         response: {
             200: S.object().prop('message', S.string()).required()
         }
     }
+};
+
+module.exports = async server => {
+    server.get('/', options, async (request, reply) => {
+        await reply.send({ message: 'hello world' });
+    });
 };
