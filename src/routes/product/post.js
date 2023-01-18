8885d7124211c239fae4bcb17fceb9a8e54dbe65
MarioWork <mariovieira.work@hotmail.com>
2023-01-18 18:14:27 -0100

    create product post route

---
+++ src/routes/product/post.js
@@ -0,0 +1,34 @@
+const S = require('fluent-json-schema');
+
+const productSchema = require('../../schemas/product-schema');
+const { createProduct } = require('../../controllers/product-controller');
+
+const schema = {
+    body: S.object()
+        .additionalProperties(false)
+        .prop('name', S.string())
+        .prop('quantity', S.number())
+        .prop('categories', S.array().items(S.number())),
+    response: {
+        201: productSchema
+    }
+};
+
+const options = { schema };
+
+module.exports = async server => {
+    const { prisma, to } = server;
+
+    server.post('/', options, async (request, reply) => {
+        const { name, categories, quantity } = request.body;
+
+        const [error, product] = to(createProduct(prisma, { name, quantity, categories }));
+
+        if (error) {
+            server.log.error(error);
+            await reply.internalServerError();
+        }
+
+        await reply.code(201).send(product);
+    });
+};
