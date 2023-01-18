90954377c7df5780f677b44a72ba6a4ef9ac83ca
MarioWork <mariovieira.work@hotmail.com>
2023-01-18 18:15:41 -0100

    fix a bug in schema

--- src/schemas/product-schema.js
+++ src/schemas/product-schema.js
@@ -4,7 +4,7 @@ const categorySchema = require('../schemas/category-schema');
 const productSchema = S.object()
     .prop('id', S.number())
     .prop('name', S.string())
-    .categories('categories', S.array().items(categorySchema))
+    .prop('categories', S.array().items(categorySchema))
     .required(['id', 'name', 'categories']);
 
 module.exports = productSchema;
