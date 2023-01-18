aac1a3fb559ff106675a55833698b10132dc3508
MarioWork <mariovieira.work@hotmail.com>
2023-01-18 18:27:54 -0100

    add quantity to schema

--- src/schemas/product-schema.js
+++ src/schemas/product-schema.js
@@ -4,6 +4,7 @@ const categorySchema = require('../schemas/category-schema');
 const productSchema = S.object()
     .prop('id', S.number())
     .prop('name', S.string())
+    .prop('quantity', S.string())
     .prop('categories', S.array().items(categorySchema))
     .required(['id', 'name', 'categories']);
 
