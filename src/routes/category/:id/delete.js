61eb0405ca2a95f68a9ff339f22c35e6e8debe61
MarioWork <mariovieira.work@hotmail.com>
2023-01-19 20:04:47 -0100

    add todo

--- src/routes/category/:id/delete.js
+++ src/routes/category/:id/delete.js
@@ -2,6 +2,7 @@ const schema = {};
 
 const options = { schema };
 
+//TODO: Implement this route
 module.exports = async server => {
     server.delete('/', options, async (request, reply) => {
         await reply.code(200).send({ message: 'In development...' });
