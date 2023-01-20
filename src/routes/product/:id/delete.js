96fbcf7a7f1c4a5ffec823a424d116a6d1fe0d11
MarioWork <mariovieira.work@hotmail.com>
2023-01-20 20:15:48 -0100

    create route to delete product

---
+++ src/routes/product/:id/delete.js
@@ -0,0 +1,35 @@
+const S = require('fluent-json-schema');
+
+const { deleteProducts } = require('../../../controllers/product-controller');
+
+const schema = {
+    response: {
+        200: S.object().prop('message', S.string()).required(['message'])
+    },
+    params: S.object().prop('id', S.number()).required(['id'])
+};
+
+const options = { schema };
+
+module.exports = async server => {
+    const { prisma, to } = server;
+
+    server.delete('/', options, async (request, reply) => {
+        const { id } = request.params;
+
+        const [error, { count }] = await to(deleteProducts(prisma, [id]));
+
+        if (count === 0) {
+            await reply.notFound();
+            return;
+        }
+
+        if (error) {
+            server.log.error(error);
+            await reply.internalServerError();
+            return;
+        }
+
+        await reply.code(200).send({ message: 'Delete successfully!' });
+    });
+};
