c7d0442db7586aa7c5fcd2b06e80faec24383cad
MarioWork <mariovieira.work@hotmail.com>
2023-01-21 18:48:35 -0100

    create jsdocs for a few functions in category controller

--- src/controllers/category-controller.js
+++ src/controllers/category-controller.js
@@ -1,3 +1,10 @@
+/**
+ * Creates a category with the given params
+ * @param {import('@prisma/client').PrismaClient} prisma - ORM Dependency
+ * @param {{name: string} } newCategory - Object that represents the category to be added
+ * @returns {Promise.<import('@prisma/client').Category | error>} - Promise object that returns the created category or error
+ * @throws
+ */
 const createCategory = (prisma, newCategory) => {
     const { name } = newCategory;
 
@@ -12,6 +19,11 @@ const createCategory = (prisma, newCategory) => {
     });
 };
 
+/** Retrieves all categories
+ * @param {import('@prisma/client').PrismaClient} prisma - ORM Dependency
+ * @returns {Promise.<import('@prisma/client').Category[] | error>} - Promise object that returns array of categories or error
+ * @throws
+ */
 const getAllCategories = prisma => {
     return prisma.category.findMany({
         select: {
@@ -21,6 +33,12 @@ const getAllCategories = prisma => {
     });
 };
 
+/**
+ * Retrieves a category by the Id given
+ * @param {import('@prisma/client').PrismaClient} prisma - ORM Dependency
+ * @param {number} id - Id of the category to retrieve
+ * @returns {Promise.<import('@prisma/client').Category | error>} - Promise object that returns category or error
+ */
 const getCategoryById = async (prisma, id) => {
     return prisma.category.findUnique({
         where: {
