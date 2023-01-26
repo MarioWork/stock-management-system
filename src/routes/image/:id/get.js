e8f0c32462f8966d7460eac5ffe71dc4c8d06833
MarioWork <mariovieira.work@hotmail.com>
2023-01-26 22:44:07 -0100

    get type from query

--- src/routes/image/:id/get.js
+++ src/routes/image/:id/get.js
@@ -2,9 +2,9 @@
 module.exports = async server => {
     server.get('/', {}, async (request, reply) => {
         const { id } = request.params;
-        const { filename } = request.query;
+        const { type } = request.query;
 
-        const fileBuffer = await server.downloadFile(id, filename);
+        const fileBuffer = await server.downloadFile(id, type);
 
         reply.type('image');
         await reply.send(fileBuffer[0]);
