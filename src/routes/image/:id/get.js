d5a53ec393cd57b49c470b9adfadf769d659e485
MarioWork <mariovieira.work@hotmail.com>
2023-01-27 16:12:48 -0100

    add schema

--- src/routes/image/:id/get.js
+++ src/routes/image/:id/get.js
@@ -1,6 +1,23 @@
-//TODO: Add schema
+const S = require('fluent-json-schema');
+
+//TODO: Move to separate file
+const AllowedFileType = {
+    PNG: 'png',
+    JPG: 'jpg',
+    JPEG: 'jpeg'
+};
+
+const schema = {
+    params: S.object().prop('id', S.string()).required(['id']),
+    query: S.object()
+        .prop('type', S.string().enum(Object.values(AllowedFileType)))
+        .required(['type'])
+};
+
+const options = { schema };
+
 module.exports = async server => {
-    server.get('/', {}, async (request, reply) => {
+    server.get('/', options, async (request, reply) => {
         const { id } = request.params;
         const { type } = request.query;
 
