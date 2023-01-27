48f24b1ad2eefa9c627f6938fb1046a4ae860dfb
MarioWork <mariovieira.work@hotmail.com>
2023-01-27 17:53:06 -0100

    check if file exists

--- src/routes/image/:id/get.js
+++ src/routes/image/:id/get.js
@@ -1,6 +1,6 @@
 const S = require('fluent-json-schema');
 
-const { AllowedFileType } = require('../../../enums/allowed-files-type');
+const { AllowedFileType } = require('../../../enums/allowed-file-type');
 
 const schema = {
     params: S.object().prop('id', S.string()).required(['id']),
@@ -12,12 +12,22 @@ const schema = {
 const options = { schema };
 
 module.exports = async server => {
+    const { to, downloadFile } = server;
     server.get('/', options, async (request, reply) => {
         const { id } = request.params;
         const { type } = request.query;
 
-        const fileBuffer = await server.downloadFile(id, type);
-        //TODO: Add error if the file does not exist
+        const [error, fileBuffer] = await to(downloadFile(id, type));
+
+        if (error) {
+            if (error.code === 404) {
+                await reply.notFound();
+                return;
+            }
+            server.log.error(error);
+            await reply.internalServerError();
+            return;
+        }
 
         reply.type('image');
         await reply.send(fileBuffer[0]);
