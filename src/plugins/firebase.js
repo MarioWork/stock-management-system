a5883cf3fd2029ef23fb26107bd86aca791646a2
MarioWork <mariovieira.work@hotmail.com>
2023-01-28 17:42:10 -0100

    remove logic for saving and downloading files to a service

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,9 +1,4 @@
 require('dotenv').config();
-const util = require('util');
-const { pipeline } = require('stream');
-const { randomUUID } = require('crypto');
-
-const pipelineAsync = util.promisify(pipeline);
 
 const fp = require('fastify-plugin');
 
@@ -24,23 +19,7 @@ const plugin = async server => {
 
         const storageBucket = getStorage().bucket();
 
-        //TODO: Move this to firebase service
-        server.decorate('saveFile', async (file, type) => {
-            const randomID = randomUUID();
-
-            const fileName = randomID + '.' + type;
-
-            const fileRef = storageBucket.file('images/' + fileName);
-
-            await pipelineAsync(file, fileRef.createWriteStream(fileName));
-
-            return process.env.IMAGE_BASE_URL + randomID + '?type=' + type;
-        });
-
-        //TODO: MOve this to firebase service
-        server.decorate('downloadFile', async (fileId, type) => {
-            return await storageBucket.file('images/' + fileId + '.' + type).download();
-        });
+        server.decorate('storage', storageBucket);
     } catch (error) {
         server.log.error(error);
     }
