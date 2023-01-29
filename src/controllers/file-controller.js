a6558e70c0942b5c7e069052619e28cba1a10a73
MarioWork <mariovieira.work@hotmail.com>
2023-01-29 16:27:29 -0100

    update docs

--- src/controllers/file-controller.js
+++ src/controllers/file-controller.js
@@ -1,4 +1,16 @@
+/**
+ * @typedef { import("../types/prisma-docs-type") } PrismaClient
+ * @typedef { import("../types/file-docs-type") } File
+ */
+
 //TODO: Move to service
+/**
+ * Retrieves a file info by Id
+ * @param {PrismaClient} prisma - ORM Dependency
+ * @param {string} id - File UUID
+ * @returns {Promise<File>}
+ * @throws {error}
+ */
 const getFile = (prisma, id) => {
     return prisma.file.findUnique({
         where: {
