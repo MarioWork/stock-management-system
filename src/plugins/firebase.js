a435c0975d7530b59c393c389da0e9aa90001ba9
MarioWork <mariovieira.work@hotmail.com>
2023-02-01 19:19:58 -0100

    complements previous commit

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const fp = require('fastify-plugin');
 
 const { initializeApp, cert } = require('firebase-admin/app');
-const { getStorage } = require('firebase-admin/storage');
 
 const { PluginNames } = require('../enums/plugins');
 const serviceAccount = require('../../credentials.json');
@@ -12,14 +11,10 @@ const plugin = async server => {
     server.log.info(`Registering ${PluginNames.FIREBASE} plugin...`);
 
     try {
-        initializeApp({
+        await initializeApp({
             credential: cert(serviceAccount),
             storageBucket: process.env.STORAGE_BUCKET
         });
-
-        const storageBucket = getStorage().bucket();
-
-        server.decorate('storage', storageBucket);
     } catch (error) {
         server.log.error(error);
     }
