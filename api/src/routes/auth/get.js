a7d855429919611547e897db5b8d0dbf94af9628
MarioWork <mariovieira.work@hotmail.com>
2023-02-02 14:31:09 -0100

    add schema

--- api/src/routes/auth/get.js
+++ api/src/routes/auth/get.js
@@ -2,10 +2,16 @@ const S = require('fluent-json-schema');
 
 const { authorize } = require('../../services/authentication/token-service');
 
+const schema = {
+    headers: S.object().prop('authorization', S.string()).required(['authorization'])
+};
+
+const options = { schema };
+
 module.exports = async server => {
     const { authService, to } = server;
 
-    server.get('/', {}, async (request, reply) => {
+    server.get('/', options, async (request, reply) => {
         const token = request.headers.authorization.split(' ')[1];
 
         const [error, decodedToken] = await to(authorize(authService, { token }));
