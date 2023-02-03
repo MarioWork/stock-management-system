4eec437dfe3f461ce6d0b31c424af042f2b42a08
MarioWork <mariovieira.work@hotmail.com>
2023-02-03 15:32:31 -0100

    add better errors

--- api/src/controllers/authentication-controller.js
+++ api/src/controllers/authentication-controller.js
@@ -1,23 +1,24 @@
+const { Forbidden } = require('http-errors');
+
 const { addUserRole: addUserRoleService } = require('../services/authentication/token-service');
 
 const decodeToken = async (authService, token) => await authService.verifyIdToken(token);
 
 //TODO: Add docs
-//TODO: ADd better errors
 const authorize = (authService, roles) => async (request, _, done) => {
     const token = request.headers.authorization.split(' ')[1];
 
-    if (!token) throw 403;
+    if (!token) throw new Forbidden('Missing authorization token');
 
     const user = await decodeToken(authService, token);
 
     //TODO: check for claims
-    if (!user) throw 403;
+    if (!user) throw new Forbidden('Invalid authorization token');
 
     //const isAuthorized = roles.every(role => !user.claims?.roles.includes(role));
     const isAuthorized = true;
 
-    if (!isAuthorized) throw 403;
+    if (!isAuthorized) throw new Forbidden('Not Authorized');
 
     request.user = user;
 
