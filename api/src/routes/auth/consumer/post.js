4e5716ea6c031a139454a9f3065fa12e5192e4a9
MarioWork <mariovieira.work@hotmail.com>
2023-02-04 20:03:30 -0100

    create route to create consumer user

---
+++ api/src/routes/auth/consumer/post.js
@@ -0,0 +1,24 @@
+const S = require('fluent-json-schema');
+
+const { UserRoles } = require('../../../enums/user-roles');
+const { authorize, createUser } = require('../../../controllers/user-controller');
+
+const schema = {
+    headers: S.object().prop('authorization', S.string()).required(['authorization'])
+};
+
+const options = authService => ({
+    schema,
+    preValidation: authorize(authService, [])
+});
+
+module.exports = async server => {
+    const { authService, prisma, to } = server;
+    server.post('/', options(authService), async (request, reply) => {
+        const [error, user] = await to(
+            createUser(prisma, { id: request.user?.uid, roles: [UserRoles.CONSUMER] })
+        );
+
+        await reply.code(200).send({ user: request.user });
+    });
+};
