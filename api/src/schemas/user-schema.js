b5b8db14d7a326be675ef8f6c54a2bf26db3e122
MarioWork <mariovieira.work@hotmail.com>
2023-02-06 22:02:42 -0100

    update user schea

--- api/src/schemas/user-schema.js
+++ api/src/schemas/user-schema.js
@@ -1,10 +1,13 @@
 const S = require('fluent-json-schema');
 
 const UserSchema = S.object()
-    .prop('uid', S.string())
-    .prop('name', S.string())
+    .prop('id', S.string())
+    .prop('firstName', S.string())
+    .prop('lastName', S.string())
+    .prop('nif', S.string())
+    .prop('profilePicture', S.string())
     .prop('email', S.string())
     .prop('roles', S.array().items(S.string()))
-    .required(['uid', 'name', 'email', 'roles']);
+    .required(['id', 'name', 'email', 'roles', 'firstName', 'lastName', 'nif', 'profilePicture']);
 
 module.exports = UserSchema;
