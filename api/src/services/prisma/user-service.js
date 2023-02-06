d9de2412506725162da030dea85d3617819090e8
MarioWork <mariovieira.work@hotmail.com>
2023-02-06 22:03:04 -0100

    update user prisma service with new fields

--- api/src/services/prisma/user-service.js
+++ api/src/services/prisma/user-service.js
@@ -1,12 +1,13 @@
 /**
  * @typedef { import('../types/product-docs-type') } PrismaClient
+ * @typedef { import('../types/user-docs-type') } User
  */
 
 /**
  * Get a user record from database with its id and role
  * @param {PrismaClient} prisma - ORM Dependency
  * @param {string} id - User ID
- * @returns {Promise<{id: string, roles: string}>} Promise when resolved returns user
+ * @returns {Promise<{User}>} Promise when resolved returns user
  * @throws {error}
  */
 const getUser = (prisma, id) => {
@@ -16,6 +17,15 @@ const getUser = (prisma, id) => {
         },
         select: {
             id: true,
+            firstName: true,
+            lastName: true,
+            nif: true,
+            profilePicture: {
+                select: {
+                    url: true
+                }
+            },
+            email: true,
             roles: true
         }
     });
@@ -25,19 +35,32 @@ const getUser = (prisma, id) => {
  * Creates a user record on database with its id and role
  * @param {PrismaClient} prisma - ORM Dependency
  * @param {string} id - User ID
- * @returns {Promise<{id: string, roles: string}>} Promise when resolved returns user
+ * @returns {Promise<{User}>} Promise when resolved returns user
  * @throws {error}
  */
-const createUser = (prisma, { id, roles }) => {
+const createUser = (prisma, { id, firstName, lastName, nif, email, roles }) => {
     return prisma.user.create({
         data: {
             id,
+            firstName,
+            lastName,
+            nif,
+            email,
             roles: {
                 set: roles
             }
         },
         select: {
             id: true,
+            firstName: true,
+            lastName: true,
+            nif: true,
+            profilePicture: {
+                select: {
+                    url: true
+                }
+            },
+            email: true,
             roles: true
         }
     });
