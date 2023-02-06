3ff3f4dadcb426f278e003bffaf1f1639b4d0205
MarioWork <mariovieira.work@hotmail.com>
2023-02-05 23:40:45 -0100

    add docs getUser prisma service

--- api/src/services/prisma/user-service.js
+++ api/src/services/prisma/user-service.js
@@ -1,4 +1,14 @@
-//TODO: Add docs
+/**
+ * @typedef { import('../types/product-docs-type') } PrismaClient
+ */
+
+/**
+ * Creates a user record on database with its id and role
+ * @param {PrismaClient} prisma - ORM Dependency
+ * @param {string} id - User ID
+ * @returns {Promise<{id: string, roles: string}>} Promise when resolved returns user
+ * @throws {error}
+ */
 const getUser = (prisma, id) => {
     return prisma.user.findUnique({
         where: {
