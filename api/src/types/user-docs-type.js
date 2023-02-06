47f731a6dc6a466a3627f125bd882b7ca3a9b3f7
MarioWork <mariovieira.work@hotmail.com>
2023-02-06 22:02:30 -0100

    create docs type user

---
+++ api/src/types/user-docs-type.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {
+ * {
+ *  id: number,
+ *  firstName: string,
+ *  lastName: string,
+ *  nif: string,
+ *  profilePicture: string,
+ *  email: roles,
+ *  roles: string[]
+ * }
+ * } User
+ */
