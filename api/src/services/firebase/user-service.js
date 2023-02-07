a76ee3591f6dc336d91cf159cbfd4000f7d02831
MarioWork <mariovieira.work@hotmail.com>
2023-02-07 02:34:32 -0100

    document

--- api/src/services/firebase/user-service.js
+++ api/src/services/firebase/user-service.js
@@ -1,4 +1,9 @@
-//TODO: add docs
+/**
+ * Signs up a user in the firebase authentication service
+ * @param {*} authService - Firebase auth service
+ * @param {{email: string, password: string}} object - Object with user email and password
+ * @returns {Promise<{{uid:string, email: string}}>}
+ */
 const createUser = (authService, { email, password }) => {
     return authService.createUser({
         email,
