aea2dee0a1996e5ac3b2e0afb84a5bda302b9225
MarioWork <mariovieira.work@hotmail.com>
2023-02-09 18:48:41 -0100

    untested route for adding profile picture

--- api/src/routes/me/picture/post.js
+++ api/src/routes/me/picture/post.js
@@ -1,19 +1,50 @@
+const S = require('fluent-json-schema');
+
 const { authorize } = require('../../../controllers/user-controller');
 const { UserRoles } = require('../../../enums/user-roles');
 
-const schema = {};
+const { AllowedFileType } = require('../../../enums/allowed-file-type');
+const { addProfilePicture } = require('../../../controllers/user-controller');
+
+const schema = {
+    response: {
+        200: S.object()
+    }
+};
 const options = ({ prisma, authService }) => ({
     schema,
     preValidation: authorize({ prisma, authService }, [UserRoles.ADMIN, UserRoles.EMPLOYEE])
 });
 
 module.exports = async server => {
-    const { prisma, to, authService } = server;
+    const { prisma, storage, to, authService } = server;
 
-    server.post(
-        '/',
-        options({ prisma, authService }, async (request, reply) => {
-            await reply.send();
-        })
-    );
+    server.post('/', options({ prisma, authService }), async (request, reply) => {
+        const { mimetype, filename, file } = await request.file();
+        const fileType = mimetype.split('/')[1]?.toLowerCase();
+
+        //If there is not file content
+        if (filename === '') {
+            await reply.badRequest('Missing file content');
+            return;
+        }
+
+        //If the file type is not allowed
+        if (!Object.values(AllowedFileType).includes(fileType)) {
+            await reply.badRequest('File type not allowed');
+            return;
+        }
+
+        const [error, user] = await to(
+            addProfilePicture({ prisma, storage }, { userId: request.user?.id, file, fileType })
+        );
+
+        if (error) {
+            server.log.error(error);
+            await reply.internalServerError();
+            return;
+        }
+
+        await reply.code(200).send(user);
+    });
 };
