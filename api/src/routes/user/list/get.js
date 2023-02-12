cb7f2b3543f1bf6f5c86c9a6bc231cbb113b249a
MarioWork <mariovieira.work@hotmail.com>
2023-02-12 21:55:04 -0100

    create route to retrieve all users

---
+++ api/src/routes/user/list/get.js
@@ -0,0 +1,33 @@
+const S = require('fluent-json-schema');
+
+const { listAllUsers, authorize } = require('../../../controllers/user-controller');
+
+const UserSchema = require('../../../schemas/user-schema');
+
+const { UserRoles } = require('../../../enums/user-roles');
+
+const schema = {
+    response: { 200: S.array().items(UserSchema) }
+};
+
+const options = ({ prisma, authService }) => ({
+    schema,
+    preValidation: authorize({ authService, prisma }, [UserRoles.ADMIN])
+});
+
+module.exports = async server => {
+    const { prisma, authService, to } = server;
+
+    server.get('/', options({ prisma, authService }), async (request, reply) => {
+        const role = Object.keys(request.query).length === 0 ? null : request.query.role;
+        const [error, users] = await to(listAllUsers(prisma, role));
+
+        if (error) {
+            server.log.error(error);
+            await reply.internalServerError();
+            return;
+        }
+
+        await reply.code(200).send(users);
+    });
+};
