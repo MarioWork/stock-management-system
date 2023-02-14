f8b67983e242fea4c120a7314512ca8576ed313c
MarioWork <mariovieira.work@hotmail.com>
2023-02-14 08:35:08 -0100

    validate post product route

--- api/src/routes/product/post.js
+++ api/src/routes/product/post.js
@@ -1,6 +1,10 @@
 const S = require('fluent-json-schema');
 
 const productSchema = require('../../schemas/product-schema');
+
+const { UserRoles } = require('../../enums/user-roles');
+
+const { authorize } = require('../../controllers/user-controller');
 const { createProduct } = require('../../controllers/product-controller');
 
 const schema = {
@@ -14,12 +18,15 @@ const schema = {
     }
 };
 
-const options = { schema };
+const options = ({ prisma, authService }) => ({
+    schema,
+    preValidation: authorize({ authService, prisma }, [UserRoles.EMPLOYEE, UserRoles.ADMIN])
+});
 
 module.exports = async server => {
-    const { prisma, to } = server;
+    const { prisma, to, authService } = server;
 
-    server.post('/', options, async (request, reply) => {
+    server.post('/', options({ prisma, authService }), async (request, reply) => {
         const { name, quantity, categories } = request.body;
 
         const [error, product] = await to(
