9f4c2120250089daa9ab15819ddc4f427f6e5d9b
MarioWork <mariovieira.work@hotmail.com>
2023-02-14 08:28:05 -0100

    validate route delete image

--- api/src/routes/image/:id/delete.js
+++ api/src/routes/image/:id/delete.js
@@ -1,16 +1,23 @@
-const { deleteFile } = require('../../../controllers/file-controller');
 const S = require('fluent-json-schema');
 
+const { UserRoles } = require('../../../enums/user-roles');
+
+const { deleteFile } = require('../../../controllers/file-controller');
+const { authorize } = require('../../../controllers/user-controller');
+
 const schema = {
-    params: S.object().prop('id', S.number()).required(['id'])
+    params: S.object().prop('id', S.string()).required(['id'])
 };
 
-const options = { schema };
+const options = ({ prisma, authService }) => ({
+    schema,
+    preValidation: authorize({ authService, prisma }, [UserRoles.EMPLOYEE, UserRoles.ADMIN])
+});
 
 module.exports = async server => {
-    const { to, storage, prisma } = server;
+    const { to, storage, prisma, authService } = server;
 
-    server.delete('/', options, async (request, reply) => {
+    server.delete('/', options({ prisma, authService }), async (request, reply) => {
         const { id } = request.params;
 
         const [error] = await to(deleteFile({ prisma, storage }, id));
