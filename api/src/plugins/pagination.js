8c4681921c16727d5298ee0d0f2044995af6b037
MarioWork <mariovieira.work@hotmail.com>
2023-02-16 21:37:44 -0100

    fix some bugs with parsePagination and create pagination reply

--- api/src/plugins/pagination.js
+++ api/src/plugins/pagination.js
@@ -8,19 +8,31 @@ const plugin = (server, _, done) => {
         const { query } = this;
 
         return {
+            get currentPage() {
+                const queryPage = Number.parseInt(query.page, 10);
+                return Number.isNaN(queryPage) ? 1 : queryPage;
+            },
             get querySize() {
                 const querySize = Number.parseInt(query.size, 10);
                 return Number.isNaN(querySize) ? DEFAULT_PAGE_SIZE : querySize;
             },
             get recordsToSkip() {
-                const queryPage = Number.parseInt(query.page, 10) - 1;
-                const page = Number.isNaN(queryPage) ? 0 : queryPage;
-                return page === 0 ? page : page * this.take;
+                const page = this.currentPage - 1;
+                return page === 0 ? page : page * this.querySize;
             }
         };
     });
 
-    server.decorateReply('withPagination', function () {});
+    server.decorateReply('withPagination', function ({ total, page, size, data }) {
+        return this.send({
+            _metadata: {
+                page,
+                size,
+                total
+            },
+            data
+        });
+    });
 
     done();
 };
