306d193deee08237896e52f1cb3563a488da14eb
MarioWork <mariovieira.work@hotmail.com>
2023-02-19 00:16:06 -0100

    centralize file fields schemas

--- api/src/schemas/file-schema.js
+++ api/src/schemas/file-schema.js
@@ -1,9 +1,15 @@
 const S = require('fluent-json-schema');
 
+const { AllowedFileType } = require('../enums/allowed-file-type');
+
+const fileIdSchema = S.string().format('uuid');
+const fileUrlSchema = S.string().format('url');
+const fileTypeSchema = S.string().enum(Object.values(AllowedFileType));
+
 const fileSchema = S.object()
-    .prop('id', S.string())
-    .prop('url', S.string())
-    .prop('type', S.string())
+    .prop('id', fileIdSchema)
+    .prop('url', fileUrlSchema)
+    .prop('type', fileTypeSchema)
     .required(['url']);
 
 module.exports = fileSchema;
