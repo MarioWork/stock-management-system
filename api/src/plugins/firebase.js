001a905731a303e0ba868d434cef791a9cc55d60
MarioWork <mariovieira.work@hotmail.com>
2023-02-19 20:28:28 -0100

    add firebase service account to env

--- api/src/plugins/firebase.js
+++ api/src/plugins/firebase.js
@@ -5,10 +5,22 @@ const fp = require('fastify-plugin');
 const { initializeApp, cert } = require('firebase-admin/app');
 
 const { PluginNames } = require('../enums/plugins');
-const serviceAccount = require('../../credentials.json');
 
 const plugin = (server, _, done) => {
     server.log.info(`Registering ${PluginNames.FIREBASE} plugin...`);
+
+    const serviceAccount = {
+        type: process.env.SERVICE_ACCOUNT_TYPE,
+        project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
+        private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+        private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
+        client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+        client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
+        auth_uri: process.env.SERVICE_ACCOUNT_AUTH_URI,
+        token_uri: process.env.SERVICE_ACCOUNT_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL
+    };
 
     try {
         initializeApp({
