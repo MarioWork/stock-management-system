e20ee94faf27bc6e166ee9e12c40f4439ae717d5
MarioWork <mariovieira.work@hotmail.com>
2023-02-22 13:41:02 -0100

    update schemas

--- api/src/schemas/category-schema.js
+++ api/src/schemas/category-schema.js
@@ -4,13 +4,21 @@ const categoryIdSchema = S.string().format('uuid');
 
 const categoryNameSchema = S.string();
 
+const categoryCreatedAtSchema = S.string().format('date-time');
+
+const categoryUpdatedAtSchema = S.string().format('date-time');
+
 const categorySchema = S.object()
     .prop('id', categoryIdSchema)
     .prop('name', categoryNameSchema)
+    .prop('createdAt', categoryCreatedAtSchema)
+    .prop('updatedAt', categoryUpdatedAtSchema)
     .required(['id', 'name']);
 
 module.exports = {
     categoryIdSchema,
     categoryNameSchema,
-    categorySchema
+    categorySchema,
+    categoryCreatedAtSchema,
+    categoryUpdatedAtSchema
 };
--- api/src/schemas/product-schema.js
+++ api/src/schemas/product-schema.js
@@ -12,12 +12,18 @@ const productQuantitySchema = S.number();
 
 const productImagesSchema = S.array().items(fileSchema);
 
+const productCreatedAtSchema = S.string().format('date-time');
+
+const productUpdatedAtSchema = S.string().format('date-time');
+
 const productCategoriesSchema = S.array().items(categorySchema);
 
 const productSchema = S.object()
     .prop('id', productIdSchema)
     .prop('name', productNameSchema)
     .prop('quantity', productQuantitySchema)
+    .prop('createdAt', productCreatedAtSchema)
+    .prop('updatedAt', productUpdatedAtSchema)
     .prop('images', productImagesSchema)
     .prop('categories', productCategoriesSchema)
     .prop('supplier', supplierSchema)
@@ -27,6 +33,8 @@ module.exports = {
     productIdSchema,
     productNameSchema,
     productQuantitySchema,
+    productCreatedAtSchema,
+    productUpdatedAtSchema,
     productImagesSchema,
     productCategoriesSchema,
     productSchema
