98188e47f2ccf7f22f011ff1612e6d90945dad02
MarioWork <mariovieira.work@hotmail.com>
2023-02-22 13:42:13 -0100

    fix schemas

--- api/src/schemas/supplier-schema.js
+++ api/src/schemas/supplier-schema.js
@@ -6,10 +6,16 @@ const supplierNameSchema = S.string().minLength(2).maxLength(40);
 
 const supplierNifSchema = S.string().minLength(9).maxLength(9);
 
+const supplierCreatedAtSchema = S.string().format('date-time');
+
+const supplierUpdatedAtSchema = S.string().format('date-time');
+
 const supplierSchema = S.object()
     .prop('id', supplierIdSchema)
     .prop('name', supplierNameSchema)
     .prop('nif', supplierNifSchema)
+    .prop('updatedAt', supplierUpdatedAtSchema)
+    .prop('createdAt', supplierCreatedAtSchema)
     .required(['id', 'name', 'nif']);
 
 module.exports = {
