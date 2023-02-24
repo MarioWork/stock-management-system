7937be9a2b311eb3074cc362984234b862b7add3
MarioWork <mariovieira.work@hotmail.com>
2023-02-24 20:27:24 -0100

    create prisma supplier service

---
+++ api/src/services/prisma/supplier-service.js
@@ -0,0 +1,19 @@
+//TODO: add docs
+const getAllSuppliers = prisma => prisma.supplier.findMany();
+
+//TODO: add docs
+const getSupplierById = (prisma, id) => prisma.supplier.findUnique({ where: { id } });
+
+//TODO: add docs
+const updateSupplier = (prisma, { id, name, nif }) =>
+    prisma.supplier.update({ where: { id }, data: { name, nif } });
+
+//TODO: add docs
+const deleteProducts = (prisma, ids) => prisma.supplier.deleteMany({ where: { id: { in: ids } } });
+
+module.exports = {
+    getAllSuppliers,
+    getSupplierById,
+    updateSupplier,
+    deleteProducts
+};
