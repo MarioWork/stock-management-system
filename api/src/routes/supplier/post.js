f91d9aff0db4a74fab650833385e4439dc811f21
MarioWork <mariovieira.work@hotmail.com>
2023-02-24 23:30:08 -0100

    post supplier route

---
+++ api/src/routes/supplier/post.js
@@ -0,0 +1,52 @@
+const S = require('fluent-json-schema');
+
+const { UserRoles } = require('../../enums/user-roles');
+
+const {
+    supplierNifSchema,
+    supplierNameSchema,
+    supplierSchema
+} = require('../../schemas/supplier-schema');
+
+const { authorize } = require('../../controllers/user-controller');
+const { createSupplier } = require('../../controllers/supplier-controller');
+
+const schema = {
+    body: S.object()
+        .additionalProperties(false)
+        .prop('name', supplierNameSchema)
+        .prop('nif', supplierNifSchema)
+        .required(['name', 'nif']),
+    response: {
+        201: supplierSchema
+    }
+};
+
+const options = ({ prisma, authService }) => ({
+    schema,
+    preValidation: authorize({ authService, prisma }, [UserRoles.EMPLOYEE, UserRoles.ADMIN])
+});
+
+module.exports = async server => {
+    const { prisma, to, authService } = server;
+
+    server.post('/', options({ prisma, authService }), async (request, reply) => {
+        const { name, nif } = request.body;
+
+        const [error, product] = await to(
+            createSupplier(prisma, {
+                name,
+                nif,
+                createdBy: request.user.id
+            })
+        );
+
+        if (error) {
+            server.log.error(error);
+            await reply.internalServerError();
+            return;
+        }
+
+        await reply.code(201).send(product);
+    });
+};
