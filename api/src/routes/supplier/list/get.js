86889ff3b38ee55871f77cc0c60ef76a85cb52a2
MarioWork <mariovieira.work@hotmail.com>
2023-02-24 23:10:00 -0100

    create list suppliers route

---
+++ api/src/routes/supplier/list/get.js
@@ -0,0 +1,49 @@
+const S = require('fluent-json-schema');
+
+const paginationMetadataSchema = require('../../../schemas/pagination-metadata-schema');
+const { supplierSchema } = require('../../../schemas/supplier-schema');
+
+const { UserRoles } = require('../../../enums/user-roles');
+
+const { authorize } = require('../../../controllers/user-controller');
+const { getAllSuppliers } = require('../../../controllers/supplier-controller');
+
+const schema = {
+    response: {
+        206: S.object()
+            .prop('_metadata', paginationMetadataSchema)
+            .prop('data', S.array().items(supplierSchema))
+            .required(['data'])
+    },
+    query: S.object().prop('filter', S.string())
+};
+
+const options = ({ prisma, authService }) => ({
+    schema,
+    preValidation: authorize({ authService, prisma }, [UserRoles.EMPLOYEE, UserRoles.ADMIN])
+});
+
+module.exports = async server => {
+    const { prisma, to, authService } = server;
+
+    server.get('/', options({ prisma, authService }), async (request, reply) => {
+        const { filter } = request.query;
+        const pagination = request.parsePaginationQuery();
+
+        const [error, result] = await to(getAllSuppliers(prisma, { filter, pagination }));
+
+        const [products, total] = result;
+
+        if (error) {
+            server.log.error(error);
+            await reply.internalServerError();
+            return;
+        }
+
+        await reply.withPagination({
+            total,
+            page: pagination.currentPage,
+            data: products
+        });
+    });
+};
