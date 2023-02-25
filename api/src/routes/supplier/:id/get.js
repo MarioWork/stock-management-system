55bf9edec9534ebb5e774b646ea600aec564bc54
MarioWork <mariovieira.work@hotmail.com>
2023-02-24 23:18:37 -0100

    get supplier id -> route done

---
+++ api/src/routes/supplier/:id/get.js
@@ -0,0 +1,41 @@
+const S = require('fluent-json-schema');
+
+const { supplierSchema, supplierIdSchema } = require('../../../schemas/supplier-schema');
+
+const { UserRoles } = require('../../../enums/user-roles');
+
+const { authorize } = require('../../../controllers/user-controller');
+const { getSupplierById } = require('../../../controllers/supplier-controller');
+
+const schema = {
+    response: { 200: supplierSchema },
+    params: S.object().prop('id', supplierIdSchema).required(['id'])
+};
+
+const options = ({ prisma, authService }) => ({
+    schema,
+    preValidation: authorize({ authService, prisma }, [UserRoles.EMPLOYEE, UserRoles.ADMIN])
+});
+
+module.exports = async server => {
+    const { prisma, to, authService } = server;
+
+    server.get('/', options({ prisma, authService }), async (request, reply) => {
+        const { id } = request.params;
+
+        const [error, product] = await to(getSupplierById(prisma, id));
+
+        if (!product) {
+            await reply.notFound(`Supplier with ID: ${id} was not found`);
+            return;
+        }
+
+        if (error) {
+            server.log.error(error);
+            await reply.internalServerError();
+            return;
+        }
+
+        await reply.code(200).send(product);
+    });
+};
