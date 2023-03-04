928f9981005156b161fe4171df634b5659ffb6fb
MarioWork <mariovieira.work@hotmail.com>
2023-03-04 20:22:03 -0100

    create a route to get user roles

---
+++ api/src/routes/user/:id/roles/get.js
@@ -0,0 +1,44 @@
+const S = require('fluent-json-schema');
+
+const { getUserRoles, authorize } = require('../../../../controllers/user-controller');
+
+const { userRolesSchema, userIdSchema } = require('../../../../schemas/user-schema');
+
+const { UserRoles } = require('../../../../enums/user-roles');
+
+const schema = {
+    params: S.object().prop('id', userIdSchema).required(['id']),
+    response: {
+        200: S.object()
+            .prop('id', userIdSchema)
+            .prop('roles', userRolesSchema)
+            .required(['id', 'roles'])
+    }
+};
+
+const options = ({ prisma, authService }) => ({
+    schema,
+    preValidation: authorize({ authService, prisma }, [UserRoles.ADMIN, UserRoles.EMPLOYEE])
+});
+
+module.exports = async server => {
+    const { prisma, authService, to } = server;
+
+    server.get('/', options({ prisma, authService }), async (request, reply) => {
+        const { id } = request.params;
+
+        const [error, user] = await to(getUserRoles(prisma, id));
+        if (!user) {
+            await reply.notFound(`User with ID: ${id} was not found`);
+            return;
+        }
+
+        if (error) {
+            server.log.error(error);
+            await reply.internalServerError();
+            return;
+        }
+
+        await reply.send(user);
+    });
+};
