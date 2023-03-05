fe1a7ea512fdd5c368721f69d71c1189d038d3c3
MarioWork <mariovieira.work@hotmail.com>
2023-03-05 19:21:43 -0100

    add createdBy to file schema

--- api/src/schemas/file-schema.js
+++ api/src/schemas/file-schema.js
@@ -1,6 +1,7 @@
 const S = require('fluent-json-schema');
 
 const { AllowedFileType } = require('../enums/allowed-file-type');
+const { userSchema } = require('../schemas/user-schema');
 
 const fileIdSchema = S.string().format('uuid');
 
@@ -8,10 +9,13 @@ const fileUrlSchema = S.string().format('url');
 
 const fileTypeSchema = S.string().enum(Object.values(AllowedFileType));
 
+const fileCreatedBySchema = userSchema;
+
 const fileSchema = S.object()
     .prop('id', fileIdSchema)
+    .prop('createdBy', fileCreatedBySchema)
     .prop('url', fileUrlSchema)
     .prop('type', fileTypeSchema)
     .required(['url']);
 
-module.exports = { fileSchema, fileIdSchema, fileUrlSchema, fileTypeSchema };
+module.exports = { fileSchema, fileIdSchema, fileUrlSchema, fileTypeSchema, fileCreatedBySchema };
