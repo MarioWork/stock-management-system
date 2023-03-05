5a3835a32f9073352ff0c19a1d09edaa3d0426ac
MarioWork <mariovieira.work@hotmail.com>
2023-03-05 19:38:13 -0100

    fix missing fields in schemas

--- api/src/schemas/category-schema.js
+++ api/src/schemas/category-schema.js
@@ -1,5 +1,7 @@
 const S = require('fluent-json-schema');
 
+const { userSchema } = require('../schemas/user-schema');
+
 const categoryIdSchema = S.string().format('uuid');
 
 const categoryNameSchema = S.string();
@@ -8,10 +10,13 @@ const categoryCreatedAtSchema = S.string().format('date-time');
 
 const categoryUpdatedAtSchema = S.string().format('date-time');
 
+const categoryCreatedBySchema = userSchema;
+
 const categorySchema = S.object()
     .prop('id', categoryIdSchema)
     .prop('name', categoryNameSchema)
     .prop('createdAt', categoryCreatedAtSchema)
+    .prop('createdBy', categoryCreatedBySchema)
     .prop('updatedAt', categoryUpdatedAtSchema)
     .required(['id']);
 
@@ -20,5 +25,6 @@ module.exports = {
     categoryNameSchema,
     categorySchema,
     categoryCreatedAtSchema,
-    categoryUpdatedAtSchema
+    categoryUpdatedAtSchema,
+    categoryCreatedBySchema
 };
--- api/src/schemas/supplier-schema.js
+++ api/src/schemas/supplier-schema.js
@@ -1,5 +1,7 @@
 const S = require('fluent-json-schema');
 
+const { userSchema } = require('../schemas/user-schema');
+
 const supplierIdSchema = S.string().format('uuid');
 
 const supplierNameSchema = S.string().minLength(2).maxLength(40);
@@ -10,12 +12,15 @@ const supplierCreatedAtSchema = S.string().format('date-time');
 
 const supplierUpdatedAtSchema = S.string().format('date-time');
 
+const supplierCreatedBySchema = userSchema;
+
 const supplierSchema = S.object()
     .prop('id', supplierIdSchema)
     .prop('name', supplierNameSchema)
     .prop('nif', supplierNifSchema)
     .prop('updatedAt', supplierUpdatedAtSchema)
     .prop('createdAt', supplierCreatedAtSchema)
+    .prop('createdBy', supplierCreatedBySchema)
     .required(['id']);
 
 module.exports = {
