382831f21c7d318f61de135fdcaf4d455a169a35
MarioWork <mariovieira.work@hotmail.com>
2023-03-09 21:21:13 -0100

    export error mapper to its own file

---
+++ api/src/utils/error-mapper.js
@@ -0,0 +1,34 @@
+const { BadRequest, NotFound, Forbidden, InternalServerError } = require('http-errors');
+
+const getFieldsFromError = error => {
+    const target = error.meta?.cause ?? error.meta?.target[0];
+    return target?.match(/'(.*?)'/i);
+};
+
+const errorMapper = error => {
+    const code = error?.code ?? error?.statusCode;
+    const message = error.message;
+
+    const map = new Map();
+
+    map.set(400, new BadRequest(message));
+    map.set(403, new Forbidden(message));
+    map.set(404, new NotFound(message));
+
+    //Firebase
+    map.set('auth/id-token-expired', new Forbidden('Expired Token'));
+    map.set('auth/user-not-found', new NotFound('User not found'));
+    map.set('auth/invalid-email', new BadRequest('Invalid Email'));
+    map.set('auth/email-already-exists', new NotFound('Email already exists'));
+
+    //Prisma
+    map.set('P2025', new NotFound(`${getFieldsFromError(error)} does not exist`));
+    map.set('P2002', new BadRequest(`${getFieldsFromError(error)}' value already exist`));
+    map.set('P2016', new NotFound());
+
+    return map.get(code) ?? new InternalServerError();
+};
+
+module.exports = {
+    errorMapper
+};
