191db965e81ad7fc715045f647a1d03c11b1dfc2
MarioWork <mariovieira.work@hotmail.com>
2023-03-10 22:43:01 -0100

    create error message localization

---
+++ api/src/localization/error-messages.js
@@ -0,0 +1,22 @@
+const { ErrorCodesKeys } = require('../enums/error-codes');
+
+const ErrorMessages = new Map();
+
+ErrorMessages.set(ErrorCodesKeys.EXPIRED_TOKEN, 'Expired token');
+
+ErrorMessages.set(ErrorCodesKeys.USER_NOT_FOUND, 'User not found');
+
+ErrorMessages.set(ErrorCodesKeys.INVALID_EMAIL, 'Invalid email address');
+
+ErrorMessages.set(ErrorCodesKeys.EMAIL_EXISTS, 'Email address already exists');
+
+ErrorMessages.set(ErrorCodesKeys.FIELD_NOT_EXIST, field => `'${field}' does not exist`);
+
+ErrorMessages.set(ErrorCodesKeys.FIELD_VALUE_EXISTS, field => `'${field}' value already exists`);
+
+ErrorMessages.set(ErrorCodesKeys.MISSING_TOKEN, 'Missing authorization token');
+ErrorMessages.set(ErrorCodesKeys.INVALID_TOKEN, 'Invalid authorization token');
+ErrorMessages.set(ErrorCodesKeys.MISSING_ROLES, 'No roles to validate');
+ErrorMessages.set(ErrorCodesKeys.NOT_AUTHORIZED, 'Role not authorized');
+
+module.exports = { ErrorMessages };
