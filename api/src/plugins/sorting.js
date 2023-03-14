d7a3cc031671206a0c0ea648448df5cc0be09943
MarioWork <mariovieira.work@hotmail.com>
2023-03-14 18:37:34 -0100

    decorate request with sortingQuery

--- api/src/plugins/sorting.js
+++ api/src/plugins/sorting.js
@@ -2,6 +2,15 @@ const fp = require('fastify-plugin');
 const { PluginNames } = require('../enums/plugins');
 
 const plugin = (server, _, done) => {
+    server.decorateRequest('parseSortingQuery', function () {
+        const { sort, order } = this;
+
+        return {
+            sort,
+            order
+        };
+    });
+
     done();
 };
 
