70b671dd3c3b53b79e154f8224a51b7e75d53447
MarioWork <mariovieira.work@hotmail.com>
2023-03-15 21:11:16 -0100

    create a enum for order types

--- api/src/plugins/sorting.js
+++ api/src/plugins/sorting.js
@@ -1,13 +1,18 @@
 const fp = require('fastify-plugin');
 const { PluginNames } = require('../enums/plugins');
 
+const OrderType = {
+    DESC: 'desc',
+    ASC: 'asc'
+};
+
 const plugin = (server, _, done) => {
     server.decorateRequest('parseSortingQuery', function (allowedFields) {
         const { sort, order } = this.query;
 
         return {
             sort: allowedFields?.includes(sort.toLowerCase()) ? sort : undefined,
-            order: order !== 'desc' || order !== 'asc' ? order : undefined
+            order: order !== OrderType.DESC || order !== OrderType.ASC ? order : undefined
         };
     });
 
