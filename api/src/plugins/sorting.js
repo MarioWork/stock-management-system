2f9abb4f941e64cb523a98a4602894ccf4623088
MarioWork <mariovieira.work@hotmail.com>
2023-03-16 19:50:45 -0100

    move allowed fields to plugin

--- api/src/plugins/sorting.js
+++ api/src/plugins/sorting.js
@@ -1,8 +1,10 @@
 const fp = require('fastify-plugin');
 const { BadRequest } = require('http-errors');
-const { ErrorCodesKeys } = require('../enums/error-codes');
 
+const { ErrorCodesKeys } = require('../enums/error-codes');
 const { PluginNames } = require('../enums/plugins');
+const { Entities } = require('../enums/entities');
+
 const { ErrorMessages } = require('../localization/error-messages');
 
 const OrderType = {
@@ -10,18 +12,44 @@ const OrderType = {
     ASC: 'asc'
 };
 
+const FieldsAllowed = new Map();
+
+FieldsAllowed.set(Entities.PRODUCT, [
+    'name',
+    'upc',
+    'description',
+    'createdAt',
+    'updatedAt',
+    'quantity'
+]);
+FieldsAllowed.set(Entities.CATEGORY, ['name', 'createdAt', 'updatedAt']);
+FieldsAllowed.set(Entities.SUPPLIER, ['name', 'nif', 'createAt', 'updatedAt']);
+FieldsAllowed.set(Entities.USER, [
+    'firstName',
+    'lastName',
+    'nif',
+    'email',
+    'updatedAt',
+    'createdAt'
+]);
+
 const plugin = (server, _, done) => {
-    server.decorateRequest('parseSortingQuery', function (allowedFields) {
+    server.decorateRequest('parseSortingQuery', function (entity) {
         const { sort, order } = this.query;
 
-        if (sort && !allowedFields?.includes(sort.toLowerCase()))
+        const allowedFields = FieldsAllowed.get(entity);
+        const isFieldAllowed = !FieldsAllowed.get(entity).includes(sort);
+
+        if (sort && isFieldAllowed)
             throw new BadRequest(
                 ErrorMessages.get(ErrorCodesKeys.INVALID_SORTING_FIELD)(allowedFields)
             );
 
         return {
             sort: sort ? sort : undefined,
-            order: order !== OrderType.DESC || order !== OrderType.ASC ? order : OrderType.DESC
+            order: Object.values(OrderType).includes(order.toLowerCase())
+                ? order.toLowerCase()
+                : OrderType.DESC
         };
     });
 
